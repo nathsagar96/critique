5a737b880f5c0e75cdb02bc5bc2d1e06c406b1b2
Sagar Nath <nathsagar96@gmail.com>
2025-04-27 14:56:00 +0530

    Add entity classes for Address, OperatingHours, Photo, Restaurant, Review, TimeRange, and User with Elasticsearch annotations

---
+++ src/main/java/dev/sagar/critique/domain/entities/OperatingHours.java
@@ -0,0 +1,36 @@
+package dev.sagar.critique.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OperatingHours {
+
+  @Field(type = FieldType.Nested)
+  private TimeRange monday;
+
+  @Field(type = FieldType.Nested)
+  private TimeRange tuesday;
+
+  @Field(type = FieldType.Nested)
+  private TimeRange wednesday;
+
+  @Field(type = FieldType.Nested)
+  private TimeRange thursday;
+
+  @Field(type = FieldType.Nested)
+  private TimeRange friday;
+
+  @Field(type = FieldType.Nested)
+  private TimeRange saturday;
+
+  @Field(type = FieldType.Nested)
+  private TimeRange sunday;
+}
---
+++ src/main/java/dev/sagar/critique/repositories/RestaurantRepository.java
@@ -0,0 +1,8 @@
+package dev.sagar.critique.repositories;
+
+import dev.sagar.critique.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {}
